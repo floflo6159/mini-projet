4072f602059d5d85a519d5c88bd4c1e85c42303d
Roys <theo.royer@gmx.fr>
2022-04-21 15:17:36 +0000

    propre

--- manifest/mariadb/mysql.sh
+++ manifest/mariadb/mysql.sh
@@ -3,7 +3,7 @@
 # Secure root account
 mysql -e "UPDATE mysql.user SET Password = PASSWORD('passwordDbUniv01') WHERE User = 'root'"
 # Create database
-mysql -e "CREATE DATABASE IF NOT EXISTS webiptables"
+mysql -e "CREATE DATABASE IF NOT EXISTS webook"
 # Create application account
 mysql -e "GRANT ALL ON *.* TO 'websiteaccount'@'localhost' IDENTIFIED BY 'passwordDbUniv01' WITH GRANT OPTION;"
 # Make our changes take effect
--- src/models/db/db_mariadb.js
+++ src/models/db/db_mariadb.js
@@ -9,7 +9,7 @@ class DbMariaDB {
       host: '127.0.0.1',
       user: 'websiteaccount',
       password: 'passwordDbUniv01',
-      database: 'webiptables'
+      database: 'webook'
     };
   }
 
